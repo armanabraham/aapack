0d9ad2bd95e39821dec9699987c8872b3d6d7187
Arman Abrahamyan <a.abrahamyan@gmail.com>
2017-01-11 00:27:54 +1100

    update theme_publish1 one more time to avoid calling all ggplot2 functions explicity and instead load ggplot2 inside theme_publish1 function

--- R/PublishingThemes.R
+++ R/PublishingThemes.R
@@ -3,22 +3,25 @@
 #'
 #' It is clean, uses italics for axes text and font size
 #' @export
-theme_publish1 <- theme_bw(base_size=12, base_family='Gill Sans') +
-  theme(plot.background = element_rect(fill = "transparent",colour = NA),
-        panel.background = element_rect(fill = "transparent",colour = NA),
-        panel.border = element_blank(),
-        strip.background=element_rect(colour="white", fill="white"),
-        #axis.line = element_line(colour = "grey80"),
-        axis.line = element_line(colour = "#a9a9a9", size = 0.3),
-        axis.ticks.x = element_line(colour = "#a9a9a9", size = 0.3),
-        axis.ticks.y = element_line(colour = "#a9a9a9", size = 0.3),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.text = element_text(size=12, face="italic"),
-        #axis.title.y = element_text(hjust=-0.1),
-        axis.title = element_text(size=12, face="italic"),
-        axis.title.x = element_text(vjust=-0.1)
-  )
+
+theme_publish1 <- function(base_size=1, base_family='Gill Sans') {
+  library(ggplot2)
+  th <- theme_bw(base_size=base_size, base_family='Gill Sans')
+  th <- th +   ggplot2::theme(plot.background = element_rect(fill = "transparent",colour = NA),
+                              panel.background = element_rect(fill = "transparent",colour = NA),
+                              panel.border = element_blank(),
+                              strip.background=element_rect(colour="white", fill="white"),
+                              #axis.line = element_line(colour = "grey80"),
+                              axis.line = element_line(colour = "#a9a9a9", size = 0.3),
+                              axis.ticks.x = element_line(colour = "#a9a9a9", size = 0.3),
+                              axis.ticks.y = element_line(colour = "#a9a9a9", size = 0.3),
+                              panel.grid.major = element_blank(),
+                              panel.grid.minor = element_blank(),
+                              axis.text = element_text(size=12, face="italic"),
+                              #axis.title.y = element_text(hjust=-0.1),
+                              axis.title = element_text(size=12, face="italic"),
+                              axis.title.x = element_text(vjust=-0.1))
+}
 
 #' ggplot theme for generating publication quality graphs
 #'
